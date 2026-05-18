5e6b56093a102727e64596d7a554d94798ec50a8
Hephzibah Ranjan <hephzibahranjan@gmail.com>
2026-05-18 12:17:27 +0530

    feat:Added user routes

---
+++ backend/src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import validate from "../middlewares/validate.js";
+import authorize from "../middlewares/authorize.js";
+import { permissions } from "../utils/permissions.js";
+import { createUserValidator, deleteUserValidator, getUserValidator } from "../helpers/userValidator.js";
+import userController from "../controller/userController.js";
+
+const router=express.Router();
+
+router.post("/auth",createUserValidator, validate, userController.createUser );
+router.get("/", getUserValidator, validate, userController.getUser );
+router.delete("/", deleteUserValidator, validate, userController.deleteUser);
